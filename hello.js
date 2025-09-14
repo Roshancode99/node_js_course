9cea9e71b8e562ca24bdf0469fcee39c23679572
Roshan Chauhan <roshanchauhan266@gmail.com>
2025-09-14 22:23:12 +0530

    intro to node js

---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("hello node js");
+
+//Node.js is a runtime environment that allows you to run JavaScript outside the browser.
+// Normally, JavaScript runs in browsers (like Chrome, Firefox, etc.), but Node.js lets you run it on your computer/server, which makes it powerful for backend development.
+// It is event-driven, non-blocking (asynchronous), which makes it very fast for I/O-heavy tasks (like APIs, databases, file operations).
